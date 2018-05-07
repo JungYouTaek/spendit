af6a715852712613775e92e9a181d5e30e8bce9d
youtaekjung <jyt9319@gmail.com>
2018-05-07 22:16:04 +0900

    change

--- React/src/components/App.js
+++ React/src/components/App.js
@@ -355,7 +355,7 @@ class App extends Component {
   render() {
     return (
       <main>
-        <h1>Spendit frontend test</h1>
+        <h1>JavaScript-calendar</h1>
         <Calendar handleCalendar={this.handleCalendar} />
         <Datepicker
           handleCalendar={this.handleCalendar}
